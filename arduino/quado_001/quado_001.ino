b1eaeec4feb397bfbd76580cfff5bece97065d1b
Andreas Grahn <andreas.grahn@gmail.com>
2013-12-13 15:25:27 +0100

    Fixed compile errors.

--- arduino/quado_001/quado_001.ino
+++ arduino/quado_001/quado_001.ino
@@ -1,157 +1,149 @@
-/*
- * Copyright (C) 2012 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- // Andreas
-#include "Arduino.h"
-#include "ADK.h"
+#include <Usb.h>
+#include <AndroidAccessory.h>
 
-ADK L;
+#define LED0          13
 
-// ADK1 usb accessory strings
-#define ACCESSORY_STRING_VENDOR "Google, Inc."
-#define ACCESSORY_STRING_NAME   "DemoKit"
-#define ACCESSORY_STRING_LONGNAME "DemoKit Arduino Board"
-#define ACCESSORY_STRING_VERSION  "1.0"
-#define ACCESSORY_STRING_URL    "http://www.android.com"
-#define ACCESSORY_STRING_SERIAL "0000000012345678"
+// Device To Accessory
+#define DTA_PLAY    0x1
+#define DTA_STOP    0x2
+// Accessory To Device
+#define ATD_NIGHT   0x3
 
-void adkPutchar(char c){Serial.write(c);}
-extern "C" void dbgPrintf(const char *, ... );
+int speakerPin = 9;
+int tones[] = { 1915, 1700, 1519, 1432, 1275, 1136, 1014, 956 };
+boolean mute = true;
+int play_tone = 0;
+int play_level = 1;
 
-void setup(void)
-{
-  Serial.begin(115200);
 
-  L.adkSetPutchar(adkPutchar);
-  L.adkInit();
-  
-  // set the old accessory strings
-  L.usbSetAccessoryStringVendor(ACCESSORY_STRING_VENDOR);
-  L.usbSetAccessoryStringName(ACCESSORY_STRING_NAME);
-  L.usbSetAccessoryStringLongname(ACCESSORY_STRING_LONGNAME);
-  L.usbSetAccessoryStringVersion(ACCESSORY_STRING_VERSION);
-  L.usbSetAccessoryStringUrl(ACCESSORY_STRING_URL);
-  L.usbSetAccessoryStringSerial(ACCESSORY_STRING_SERIAL);
-  
-  L.usbStart();
+int data = 2; 
+int clock = 3;
+int latch = 4;
+
+
+// RGB LED leads connected to PWM pins
+const int RED_LED_PIN = 10;
+const int GREEN_LED_PIN = 11;
+const int BLUE_LED_PIN = 12;
+int reds[] = {0xff, 0xff, 0xff, 0x32, 0x00, 0x48, 0xee, 0xdc};
+int greens[] = {0, 0x45, 0xd7, 0xcd, 0x00, 0x3d, 0x82, 0x14};
+int blues[] = {0, 0, 0x00, 0x32, 0xff, 0x8b, 0xee, 0x3c};
+
+
+//PhotoResistor Pin
+int lightPin = 0;
+boolean night = false;
+
+
+// Potentiometer
+int potentiometerPin = 3;
+
+AndroidAccessory acc("GDG Suwon",            // Manufacturer
+                     "clavier_aoa",                 // Model
+                     "MiniHack project Arduino Board",   // Description
+                     "1.0",                     // Version
+                     "http://gdg-suwon.blogspot.kr/2012/06/gdg-suwon-hack-time-for-android-adk.html",  // URI
+                     "0000000012345678");       // Serial
+
+
+void playTone() {
+  digitalWrite(speakerPin, HIGH);
+  delayMicroseconds(play_tone / play_level);
+  digitalWrite(speakerPin, LOW);
+  delayMicroseconds(play_tone / play_level);
 }
 
-struct SendBuf {
-  void Reset() { pos = 0; memset(buf, 0, sizeof(buf)); }
-  void Append(int val) { buf[pos++] = val; }
-  void Append(uint8_t val) { buf[pos++] = val; }
-  void Append(uint16_t val) { buf[pos++] = val >> 8; buf[pos++] = val; }
-  void Append(uint32_t val) { buf[pos++] = val >> 24; buf[pos++] = val >> 16; buf[pos++] = val >> 8; buf[pos++] = val; }
+void setTone(int value) {
+  mute = false;
+  play_tone = value;
+}
 
-  int Send() { return L.accessorySend(buf, pos); }
+void turnOffTone() {
+  mute = true;
+}
 
-  uint8_t buf[128];
-  int pos;
-};
+void setPlayLevel(int value) {
+  play_level = value;
+}
+
+
+/*
+ * updateLEDs() - sends the LED states set in ledStates to the 74HC595
+ * sequence
+ */
+void updateLEDs(int value){
+  digitalWrite(latch, LOW);     //Pulls the chips latch low
+  shiftOut(data, clock, MSBFIRST, value); //Shifts out the 8 bits to the shift register
+  digitalWrite(latch, HIGH);   //Pulls the latch high displaying the data
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.print("\r\nADK Started\r\n");
+
+  pinMode(speakerPin, OUTPUT);
+  pinMode(LED0, OUTPUT);
+  
+  pinMode(data, OUTPUT);
+  pinMode(clock, OUTPUT);  
+  pinMode(latch, OUTPUT);  
+  
+  // Power On Android Accessory interface and init USB controller
+  acc.powerOn();
+}
+
 
 void loop()
 {
-  static int last_report = 0;
-  static uint16_t temp = 0;
-  static uint16_t light = 0;
-  static uint8_t buttons = 0;
-  static uint8_t joyx = 0;
-  static uint8_t joyy = 0;
-  static uint8_t ledrgb[6][3];
-
-  int now = millis() / 100;
-  
-  // see if we need to report our current status
-  if (now != last_report) {
-    SendBuf buf;
-    buf.Reset();
-
-    // temperature
-    buf.Append(0x04);
-    buf.Append(temp);
-    temp++;
-
-    // light<
-    buf.Append(0x05);
-    buf.Append(light);
-    light++;
-
-    // buttons
-    for (int i = 0; i < 5; i++) {
-      buf.Append(0x01);
-      buf.Append(i);
-      buf.Append((uint8_t)(buttons & (1 << i)));
+  byte data[2];
+  if (acc.isConnected()) {
+    int len = acc.read(data, sizeof(data), 1);
+    if (len > 0) {
+      if (data[0] == DTA_PLAY) {
+        setTone(tones[data[1]]);
+        updateLEDs(1 << data[1]);
+        analogWrite(RED_LED_PIN, reds[data[1]]);
+        analogWrite(GREEN_LED_PIN, greens[data[1]]);
+        analogWrite(BLUE_LED_PIN, blues[data[1]]);
+        Serial.println("play");
+      } else if (data[0] == DTA_STOP) {
+        turnOffTone();
+        updateLEDs(0);
+        analogWrite(RED_LED_PIN, 0);
+        analogWrite(GREEN_LED_PIN, 0);
+        analogWrite(BLUE_LED_PIN, 0);
+        Serial.println("mute");
+      }
     }
-    buttons++;
-
-    // joystick
-    buf.Append(0x06);
-    buf.Append((uint8_t)(L.capSenseSlider() - 128));
-    buf.Append(0);
-    joyx += 2;
-    joyy += 3;
-
-    buf.Send();
-  }
-
-  // read from phone
-  {
-    uint8_t buf[64];
-
-    int res = L.accessoryReceive(buf, sizeof(buf));
- 
-    int pos = 0;
-    while (pos < res) {
-      uint8_t op = buf[pos++];
-  
-      switch (op) {
-        case 0x2: {
-          // color sliders
-          unsigned int led = buf[pos];
-          
-          if (led < 12) {
-            ledrgb[led/3][led%3] = buf[pos+1]; 
-          } else if (led >= 0x10 || led <= 0x12) {
-            ledrgb[3][led%3] = buf[pos+1];            
-          }
-
-          pos += 2;
-          break;
-        }
-        case 0x3: {
-          // relay
-          int val = buf[pos+1] ? 255 : 0;
-          int led = (buf[pos] == 0) ? 4 : 5;
-          ledrgb[led][0] = val;              
-          ledrgb[led][1] = val;              
-          ledrgb[led][2] = val;              
-          pos += 2;
-          break;
-        }
-        default: // assume 3 byte packet
-          pos += 2;
-          break;
-      }    
+    
+    if (!mute) {
+      playTone();
     }
+    
+    int lightLevel = analogRead(lightPin);
+    boolean newNight = lightLevel > 700;
+    // comment next before release
+    //Serial.println(lightLevel);
+    if (night != newNight) {
+      Serial.println("time changed!");
+      Serial.println(night ? "night" : "morning");
+      night = newNight;
+      data[0] = ATD_NIGHT;
+      data[1] = night ? 0x1 : 0x0;
+      acc.write(data, 2);
+    }
+    night = newNight;
+    
+    
+    int potention = analogRead(potentiometerPin);
+    //Serial.println(potention);
+    setPlayLevel(potention / 400);
+    
   }
   
-  for (int i = 0; i < 6; i++) {
-    L.ledDrawLetter(i, '8', ledrgb[i][0], ledrgb[i][1], ledrgb[i][2]);
-  }
+  digitalWrite(LED0, LOW);
 
-  last_report = now;
-  
-  L.adkEventProcess(); //let the adk framework do its thing
+  delay(10);
 }
+
